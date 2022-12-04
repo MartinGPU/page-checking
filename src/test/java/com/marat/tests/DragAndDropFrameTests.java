0a7f18e7d08662bca7be46844941c59e70991bb2
MartinGPU <80305279+MartinGPU@users.noreply.github.com>
2022-12-04 21:24:20 +0400

    Update DragAndDropFrameTests.java

--- src/test/java/com/marat/tests/DragAndDropFrameTests.java
+++ src/test/java/com/marat/tests/DragAndDropFrameTests.java
@@ -2,7 +2,6 @@ package com.marat.tests;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Selectors.byTagName;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
